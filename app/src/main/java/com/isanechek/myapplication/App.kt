69e359be419b6aa87499d86830bbdb91704a2a4a
isanechek <neizvestniytip@gmail.com>
2019-02-28 12:18:05 +0400

    add yandex metrica

--- app/src/main/java/com/isanechek/myapplication/App.kt
+++ app/src/main/java/com/isanechek/myapplication/App.kt
@@ -6,7 +6,8 @@ import com.isanechek.myapplication.di.appModule
 import com.isanechek.myapplication.di.dataModule
 import com.isanechek.myapplication.di.utilsModule
 import com.vk.api.sdk.VK
-import com.vk.api.sdk.VKApiConfig
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
@@ -19,13 +20,22 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val yandexMetricaConfig = YandexMetricaConfig
+            .newConfigBuilder("7d379411-ba8a-4cff-ac83-cc1cb4a28e50")
+            .withAppVersion(BuildConfig.VERSION_NAME)
+            .build()
+        YandexMetrica.activate(applicationContext, yandexMetricaConfig)
+        YandexMetrica.enableActivityAutoTracking(this)
+
         VK.initialize(this)
 
 
-        startKoin(this, listOf(
-            appModule,
-            dataModule,
-            utilsModule
-        ))
+        startKoin(
+            this, listOf(
+                appModule,
+                dataModule,
+                utilsModule
+            )
+        )
     }
 }
